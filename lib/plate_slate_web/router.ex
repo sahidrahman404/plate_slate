2261e7b1db9d8dc62d862704871c4f7d63ebf8cf
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-12 16:05:11 +0700

    expose api endpoint

--- lib/plate_slate_web/router.ex
+++ lib/plate_slate_web/router.ex
@@ -12,6 +12,8 @@ defmodule PlateSlateWeb.Router do
   scope "/" do
     pipe_through :api
 
+    forward "/api", Absinthe.Plug, schema: PlateSlateWeb.Schema
+
     forward "/graphiql", Absinthe.Plug.GraphiQL,
       schema: PlateSlateWeb.Schema,
       interface: :simple,
