4e3a857fb81d4b92424c3d2688d6dec4b7da51a5
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-12 13:54:29 +0700

    add result for :menu_item resolver

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,8 +1,14 @@
 defmodule PlateSlateWeb.Schema do
+  alias PlateSlate.Menu
+  alias PlateSlate.Repo
   use Absinthe.Schema
 
   query do
-    field :menu_items, list_of(:menu_item)
+    field :menu_items, list_of(:menu_item) do
+      resolve(fn _, _, _ ->
+        {:ok, Repo.all(Menu.Item)}
+      end)
+    end
   end
 
   object :menu_item do
