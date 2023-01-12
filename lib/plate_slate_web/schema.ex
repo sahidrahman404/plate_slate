fe192eca938eefad5f5a6cf73fd930cf54f1c5ef
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-12 17:26:03 +0700

    update menu_item object

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -4,7 +4,7 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   query do
-  @desc "The list of available items on the menu" 
+    @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
       resolve(fn _, _, _ ->
         {:ok, Repo.all(Menu.Item)}
@@ -12,9 +12,12 @@ defmodule PlateSlateWeb.Schema do
     end
   end
 
+  @desc "Menu item fields"
   object :menu_item do
     field :id, :id
     field :name, :string
     field :description, :string
+    field :price, :string
+    field :added_on, :string
   end
 end
